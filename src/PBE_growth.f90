0521a90ba7f4db7a3ccfb5c108eecc792376435d
lboscagli <l.boscagli92@gmail.com>
2025-07-17 10:06:02 +0100

    debuggin growth module

--- src/PBE_growth.f90
+++ src/PBE_growth.f90
@@ -66,7 +66,7 @@ else if (growth_function>=4) then
   if (growth_function==4) then
     call pbe_freezing_temperature(index, T_frz)
     !Depositional growth model activation (Karcher et al. 1996)
-    if (((p_water .ge. p_sat_liq) .and. (current_temp > T_frz)) .or. ((p_water .ge. p_sat_ice) .and. (current_temp .le. T_frz))) then !SAC criterion
+    if (((p_water .ge. p_sat_liq))) then ! .and. (current_temp > T_frz)) .or. ((p_water .ge. p_sat_ice) .and. (current_temp .le. T_frz))) then !SAC criterion
       call pbe_depositional_growth_ice(index,ni,g_coeff1_l,g_coeff1_r,g_coeff2)   
     else
       g_coeff1_l =0.0
